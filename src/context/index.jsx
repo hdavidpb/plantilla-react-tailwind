4516e96cac9d726198a71c9bfc7fa6ebb0faf9a4
Hernan Plaza <hernan.plazabs@gmail.com>
2023-02-09 10:16:14 -0500

    second commit

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -34,7 +34,7 @@ export function MaterialTailwindControllerProvider({ children }) {
   const initialState = {
     openSidenav: false,
     sidenavColor: "blue",
-    sidenavType: "dark",
+    sidenavType: "white",
     transparentNavbar: true,
     fixedNavbar: false,
     openConfigurator: false,

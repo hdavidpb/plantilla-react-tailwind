df80502d01319eaeaf312ad3c30ebb27668b1633
Hernan Plaza <hernan.plazabs@gmail.com>
2023-02-15 16:33:18 -0500

    [UPDATE] profile page

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -4,7 +4,7 @@ const ProgressBar = ({ width }) => {
   return (
     <div className="w-full rounded-full bg-gray-200 dark:bg-gray-700">
       <div
-        className="mb-2 rounded-full p-0.5 text-center text-xs font-medium leading-none text-white"
+        className="rounded-full p-0.5 text-center text-xs font-medium leading-none text-white"
         style={{ width: `${width}%`, backgroundColor: "rgb(255, 119, 175 )" }}
       >
         {`${width}%`}
